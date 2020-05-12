9c949df0c6dc2b2e1cb34e4b4e4aa642d071731a
jhig <jhigbee14@gmail.com>
2020-05-12 11:27:07 -0500

    -Updated Pipfile to include InfluxDB package -Added influxDB.conf to allow for address binding -Updated cta_api dag to write incoming bus times to Influx DB -Started on InfluxDB hook -Added InfluxDB image to docker-compose file

--- dags/cta_api.py
+++ dags/cta_api.py
@@ -7,6 +7,7 @@ from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
+from influxdb import InfluxDBClient
 import requests
 import json
 import os
@@ -19,45 +20,70 @@ args = {
     "email": ["airflow@airflow.com"],
     "email_on_failure": False,
     "email_on_retry": False,
-    "retries": 2,
+    "retries": 1,
     "retry_delay": timedelta(minutes=1),
 }
 
 dag = DAG(dag_id='cta_api',
           default_args=args,
-          schedule_interval='*/1 * * * *',
-          dagrun_timeout=timedelta(seconds=120))
+          schedule_interval='* * * * *',
+          dagrun_timeout=timedelta(seconds=120),
+          catchup=False)
 
 def print_context(ds, **kwargs):
+ 
+    client = InfluxDBClient('influxdb', 8086, 'root', 'root', 'cta')
+
     ti = kwargs['ti']
     templates_dict = kwargs['templates_dict']
     API_KEY = templates_dict['api_key']
-    print(API_KEY)
     resp = requests.get(f'http://www.ctabustracker.com/bustime/api/v2/getpredictions?key={API_KEY}&stpid=1149&format=json')
     resp_dict = json.loads(resp.text)
 
-    print(resp_dict['bustime-response']['prd'][1])
-    print(len(resp_dict['bustime-response']['prd']))
     if resp.status_code != 200:
         raise ApiError('GET /tasks/ {}'.format(resp.status_code))
-    for x, ele in enumerate(resp_dict['bustime-response']['prd']):
-        call_time = resp_dict['bustime-response']['prd'][x]['tmstmp']
-        arr_predict_type = resp_dict['bustime-response']['prd'][x]['typ']
-        stop_name = resp_dict['bustime-response']['prd'][x]['stpnm']
-        stop_id = resp_dict['bustime-response']['prd'][x]['stpid']
-        bus_id = resp_dict['bustime-response']['prd'][x]['vid']
-        distance_to_stop = resp_dict['bustime-response']['prd'][x]['dstp']
-        route = resp_dict['bustime-response']['prd'][x]['rt']
-        direction = resp_dict['bustime-response']['prd'][x]['rtdir']
-        end_dest = resp_dict['bustime-response']['prd'][x]['des']
-        predicted_arr = resp_dict['bustime-response']['prd'][x]['prdtm']
-        tablockid = resp_dict['bustime-response']['prd'][x]['tablockid']
-        trip_id = resp_dict['bustime-response']['prd'][x]['tatripid']
-        delay = resp_dict['bustime-response']['prd'][x]['dly']
-        predicted_ct_dn = resp_dict['bustime-response']['prd'][x]['prdctdn']
-        zone = resp_dict['bustime-response']['prd'][x]['zone']
-        print('{}, {}, {}, {}, {}'.format(stop_id, route, call_time, predicted_ct_dn, bus_id))
-    return 'Whatever you return gets printed in the logs'
+    try:
+        for x, ele in enumerate(resp_dict['bustime-response']['prd']):
+ 
+            json_body = [
+                        {
+                            "measurement": "bus_time",
+                            "tags": {
+                                "host": "server01",
+                                "region": "us-west"
+                            },
+                            #"time": "2009-11-10T23:00:00Z",
+                            "fields": {
+                                        "call_time": resp_dict['bustime-response']['prd'][x]['tmstmp'],
+                                        "arr_predict_type": resp_dict['bustime-response']['prd'][x]['typ'],
+                                        "stop_name": resp_dict['bustime-response']['prd'][x]['stpnm'],
+                                        "stop_id": resp_dict['bustime-response']['prd'][x]['stpid'],
+                                        "bus_id": resp_dict['bustime-response']['prd'][x]['vid'],
+                                        "distance_to_stop": resp_dict['bustime-response']['prd'][x]['dstp'],
+                                        "route": resp_dict['bustime-response']['prd'][x]['rt'],
+                                        "direction": resp_dict['bustime-response']['prd'][x]['rtdir'],
+                                        "end_dest": resp_dict['bustime-response']['prd'][x]['des'],
+                                        "predicted_arr": resp_dict['bustime-response']['prd'][x]['prdtm'],
+                                        "tablockid": resp_dict['bustime-response']['prd'][x]['tablockid'],
+                                        "trip_id": resp_dict['bustime-response']['prd'][x]['tatripid'],
+                                        "delay": resp_dict['bustime-response']['prd'][x]['dly'],
+                                        "predicted_ct_dn": resp_dict['bustime-response']['prd'][x]['prdctdn'],
+                                        "zone": resp_dict['bustime-response']['prd'][x]['zone']
+                            }
+                        }
+                    ]
+            print(json_body)
+            client.write_points(json_body)
+
+            result = client.query('select * from bus_time;')
+
+            print("Result: {0}".format(result))
+    except IndexError as error:
+        print(f'No incoming buses (Index) {error}')
+    except KeyError as error:
+        print(f'No incoming buses (Key) {error}')
+
+    return 'Write to InfluxDB Complete'
 
 
 run_this = PythonOperator(
